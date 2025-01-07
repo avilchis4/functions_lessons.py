40b4b84032cc3cd1e61b8faeaed050e046497e66
Vilchis <avilchis4@cps.k12.il.us>
2025-01-07 09:09:25 -0600

    functionsday1

--- 1_.py
+++ 1_.py
@@ -1,5 +1,85 @@
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
+# functions are ways to wrap your code
+# into reusuable units
+
+
+# how you define a function
+# only define a function ONCE
+# whatever i pass inside the parentheses is called a paramater (place holder for future information)
+# def sayHello(name, age, number):
+#     print (f"say hello {name}")
+#     print ("say hello")
+#     print ("Hello Governor")
+#     print (f"welcome back {name}")
+#     print (f"your age is {age}")
+#     print (f"your phone number is {number}")
+
+# # once you define a function, you must call or invoke the function
+# # when i pass info into the called function, its called an argument
+# sayHello("ariana", 16, "708-347-1958")
+# sayHello("tyler", 34, "773-027-9486")
+# sayHello("hayden", 19, "708-190-0271")
+
+# def determainEligibility(age):
+#     # if your age is over the age of 18, you can vote
+#     # otherwise you cannot vote
+#     if age >= 18:
+#         print ("you can vote")
+#     else:
+#         print("you can't vote")
+
+# determainEligibility(12)
+# determainEligibility(19)
+# determainEligibility(15)
+
+# def willYouGraducate(gpa, credits, SAT):
+#     # gpa is float variable
+#     # credits is number variable
+#     # passed SAT is boolen (true or false)
+#     if (gpa >= 3.0) and (credits >= 28) and (SAT == True):
+#         print ("you passed!")
+#     elif (gpa < 3.0) or (credits < 28) or (SAT != True):
+#         print ("back to the drawing board")
+#     else:
+#         print ("talk to your counselor")
+    
+# willYouGraducate(2.8, 15, True)
+# willYouGraducate(3.7, 30, True)
+# willYouGraducate(2.8, 15, False)
+
+# return = staement used to end a function and send a result back to caller 
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# functions wont work unless you call them
+
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
 
 # ,
 
